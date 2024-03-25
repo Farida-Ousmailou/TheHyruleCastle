6b587221614f33beddab10f0d26fb7cbd820b8a4
Farida Ousmailou <faridaousmailou51@gmail.com>
2024-03-25 09:55:41 +0100

    object character and interface

---
+++ characters.ts
@@ -0,0 +1,24 @@
+
+export interface participants {
+    name : string
+    hp :  number
+    str : number
+}
+
+export const playeur : participants = {
+    name : "link",
+    hp : 60,
+    str : 15,
+}
+
+export const enemy : participants = {
+    name : "Bokoblin ",
+    hp: 30,
+    str :  5
+}
+
+export const boss : participants = {
+    name : "Ganon",
+    hp: 150,
+    str : 20 
+}
