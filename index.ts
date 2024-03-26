70234652a8c98c667f1c0174c7df2480d7c98a82
Farida Ousmailou <faridaousmailou51@gmail.com>
2024-03-26 09:11:45 +0100

    add function to handle the character hp

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import { player, enemy, boss } from './characters'
 
 
 console.log(`\twelcome to The Hyrule Castle\n \tDescription : You try to save the princess ZELDA\n`)
-console.log(`\t`, "On this game you will confront", enemy.name,"and you will represent by your hero ",playeur.name, `\n`)
+console.log(`\t`, "On this game you will confront", enemy.name,"and you will represent by your hero ",player.name, `\n`)
 console.log(`\t Good luck \n`)
 console.log(`~~~~~~~~~~You have to choose between the both options~~~~~~~~~~~\n`)
 
@@ -22,9 +22,9 @@ let str_boss = boss.str
 
 function check_getInput() {
 
-    let answer = " "
+    let answer = ""
         try {
-            while(answer_possible.includes(answer = getInput("attack 🗡 or heal ❤️ ?").toLowerCase())) {
+            while(!answer_possible.includes(answer = getInput("attack 🗡 or heal ❤️ ?").toLowerCase())) {
                 if (answer === answer_possible[0] || answer === answer_possible[1]) {
                     return answer
                 } else {
@@ -35,3 +35,16 @@ function check_getInput() {
             console.error("Désolé une erreur est servenue" + error)
         }
     }
+
+    function hp_flow (hp_hero:  number, hp_opponent: number){
+        if (hp_opponent=== 0){
+            console.log("Your opponent is died",`\n`)
+            console.log("congratulation you win the round",`\n`)
+        } else if (hp_hero === 0) {
+            console.log("You lose, Game Over",`\n`)
+        } else {
+            console.log("The game will be to continue...",`\n`)
+        }
+    }
+
+   
